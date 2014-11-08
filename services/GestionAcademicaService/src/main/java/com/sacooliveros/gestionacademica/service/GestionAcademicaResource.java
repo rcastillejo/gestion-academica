cd9417a2331d906a4bd58101e0ee33018afeb5c3
ricardo.castillejo <ricardo.castillejo@gmail.com>
2014-11-08 22:39:18 +0000

    Se agregan las fuentes del servicio GestionAcademicaService

---
+++ services/GestionAcademicaService/src/main/java/com/sacooliveros/gestionacademica/service/GestionAcademicaResource.java
@@ -0,0 +1,163 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sacooliveros.gestionacademica.service;
+
+import com.google.gson.Gson;
+import com.sacooliveros.gestionacademica.bean.AlumnoBean;
+import com.sacooliveros.gestionacademica.bean.AsistenciaBean;
+import com.sacooliveros.gestionacademica.bean.DetalleAsistenciaBean;
+import com.sacooliveros.gestionacademica.bean.NotaBean;
+import com.sacooliveros.gestionacademica.bean.SimulacroBean;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+
+/**
+ * REST Web Service
+ *
+ * @author Alumnos
+ */
+@Path("GestionAcademica")
+public class GestionAcademicaResource {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of GestionAcademicaResource
+     */
+    public GestionAcademicaResource() {
+    }
+
+    /**
+     * Funcionalidads del Catalogo
+     */
+    /**
+     * Consultar Datos
+     *
+     * @param id
+     * @return
+     */
+    @GET
+    @Path("/alumno/{id}")
+    @Produces("application/json")
+    public String getAlumno(@PathParam("id") String id) {
+        Gson gson = new Gson();
+        AlumnoBean alumno = new AlumnoBean();
+        alumno.setId("123");
+        alumno.setNombres("Luis Ricardo");
+        alumno.setApellidos("Castillejo Luna");
+        alumno.setTelefono("12345678");
+        alumno.setCorreo("rcastillejo@gmail.com");
+
+        return gson.toJson(alumno);
+    }
+
+    /**
+     * Actualizar Datos
+     *
+     * @param id
+     * @return
+     */
+    @PUT
+    @Path("/alumno")
+    @Consumes("application/json")
+    public void putAlumno(String content) {
+        Gson gson = new Gson();
+        AlumnoBean alumno = gson.fromJson(content, AlumnoBean.class);
+        System.out.println("alumno:\n" + alumno);
+    }
+
+    /**
+     * Consultar Notas
+     *
+     * @param alumnoId
+     * @return
+     */
+    @GET
+    @Path("/notas/{alumnoId}")
+    @Produces("application/json")
+    public String getNotas(@PathParam("alumnoId") String alumnoId) {
+        Gson gson = new Gson();
+
+        List<NotaBean> notas = new ArrayList<NotaBean>();
+        NotaBean nota = new NotaBean();
+        nota.setAlumnoId(alumnoId);
+        nota.setCurso("Matematicas");
+        nota.setNota(15);
+        nota.setPeriodo("1er Bimestre");
+        nota.setProfesor("Ricarod Castillejo");
+
+        notas.add(nota);
+
+        return gson.toJson(notas);
+    }
+
+    /**
+     * Consultar Resultado Simulacro
+     *
+     * @param alumnoId
+     * @return
+     */
+    @GET
+    @Path("/simulacro/{alumnoId}")
+    @Produces("application/json")
+    public String getSimulacro(@PathParam("alumnoId") String alumnoId) {
+
+        //Invocar al servicio ColegioService
+
+        Gson gson = new Gson();
+        SimulacroBean simulacro = new SimulacroBean();
+        simulacro.setAlumnoId(alumnoId);
+        simulacro.setCurso("Matematicas");
+        simulacro.setNota(15);
+        simulacro.setPeriodo("1er Bimestre");
+
+        return gson.toJson(simulacro);
+    }
+
+    /**
+     * Consultar Asistencia
+     *
+     * @param alumnoId
+     * @return
+     */
+    @GET
+    @Path("/asistencia/{alumnoId}")
+    @Produces("application/json")
+    public String getAsistencia(@PathParam("alumnoId") String alumnoId) {
+
+        Gson gson = new Gson();
+
+        DetalleAsistenciaBean dia = new DetalleAsistenciaBean();
+        dia.setAnio(2012);
+        dia.setDia(12);
+        dia.setMes(12);
+        dia.setEstado("TAR");
+
+        List dias = new ArrayList<DetalleAsistenciaBean>();
+        dias.add(dia);
+
+        AsistenciaBean asistencia = new AsistenciaBean();
+        asistencia.setAlumnoId(alumnoId);
+        asistencia.setFechaIni("12/12/12");
+        asistencia.setFechaFin("12/12/12");
+        asistencia.setTotalAsi(10);
+        asistencia.setTotalIna(10);
+        asistencia.setTotalJus(0);
+        asistencia.setTotalNom(0);
+        asistencia.setTotalTar(0);
+        asistencia.setDetalleAsistencia(dias);
+        
+        return gson.toJson(asistencia);
+    }
+}
