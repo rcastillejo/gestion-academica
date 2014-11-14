b6466d8dc572ed6727ee5ec6ae15c7701270f03c
pponce.suyo <pponce.suyo@gmail.com>
2014-11-14 05:26:15 +0000

    Actualizacion del Servico Colegio

--- services/PagoService/src/main/java/com/sacooliveros/pago/pagoservice/PagoResource.java
+++ services/PagoService/src/main/java/com/sacooliveros/pago/pagoservice/PagoResource.java
@@ -6,6 +6,8 @@ package com.sacooliveros.pago.pagoservice;
 
 import com.google.gson.Gson;
 import com.sacooliveros.pago.model.PagoBean;
+import java.util.ArrayList;
+import java.util.List;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.PathParam;
@@ -39,10 +41,11 @@ public class PagoResource {
     @GET
     @Path("/{alumnoId}")
     @Produces("application/json")
-    public String getJson(@PathParam("alumnoId") String alumnoId) {
+    public String ConsultarPensiones(@PathParam("alumnoId") String alumnoId) {
         //TODO return proper representation object
         //throw new UnsupportedOperationException();
         Gson vgson = new Gson();
+        List<PagoBean> pagos = new ArrayList<PagoBean>();
         PagoBean pago = new PagoBean();
         pago.anno = "2014";
         pago.codigo="JPEREZ";
@@ -51,14 +54,134 @@ public class PagoResource {
         pago.concepto="MATRICULA";
         pago.estado ="CANCELADO";
         pago.monto = 200;
-        pago.fechavenc="15/03/2014";
-        pago.fechapago="05/03/2014";
+        pago.fechavenc="01/03/2014";
+        pago.fechapago="22/02/2014";
         pago.nombre = "JUAN PEREZ";
+        pagos.add(pago);
         
-        
-        return vgson.toJson(pago);
-    }
+        pago.anno = "2014";
+        pago.codigo="JPEREZ";
+        pago.itemno = 2;
+        pago.mes = "MARZO";
+        pago.concepto="PENSION";
+        pago.estado ="CANCELADO";
+        pago.monto = 150;
+        pago.fechavenc="07/03/2014";
+        pago.fechapago="30/02/2014";
+        pago.nombre = "JUAN PEREZ";
+        pagos.add(pago);
 
+        pago.anno = "2014";
+        pago.codigo="JPEREZ";
+        pago.itemno = 3;
+        pago.mes = "ABRIL";
+        pago.concepto="PENSION";
+        pago.estado ="CANCELADO";
+        pago.monto = 150;
+        pago.fechavenc="07/04/2014";
+        pago.fechapago="30/03/2014";
+        pago.nombre = "JUAN PEREZ";
+        pagos.add(pago);
+
+        pago.anno = "2014";
+        pago.codigo="JPEREZ";
+        pago.itemno = 4;
+        pago.mes = "MAYO";
+        pago.concepto="PENSION";
+        pago.estado ="CANCELADO";
+        pago.monto = 150;
+        pago.fechavenc="07/05/2014";
+        pago.fechapago="30/04/2014";
+        pago.nombre = "JUAN PEREZ";
+        pagos.add(pago);
+
+        pago.anno = "2014";
+        pago.codigo="JPEREZ";
+        pago.itemno = 5;
+        pago.mes = "JUNIO";
+        pago.concepto="PENSION";
+        pago.estado ="CANCELADO";
+        pago.monto = 150;
+        pago.fechavenc="07/06/2014";
+        pago.fechapago="30/05/2014";
+        pago.nombre = "JUAN PEREZ";
+        pagos.add(pago);
+
+        pago.anno = "2014";
+        pago.codigo="JPEREZ";
+        pago.itemno = 6;
+        pago.mes = "JULIO";
+        pago.concepto="PENSION";
+        pago.estado ="CANCELADO";
+        pago.monto = 170;
+        pago.fechavenc="07/07/2014";
+        pago.fechapago="30/06/2014";
+        pago.nombre = "JUAN PEREZ";
+        pagos.add(pago);
+
+        pago.anno = "2014";
+        pago.codigo="JPEREZ";
+        pago.itemno = 7;
+        pago.mes = "AGOSTO";
+        pago.concepto="PENSION";
+        pago.estado ="CANCELADO";
+        pago.monto = 150;
+        pago.fechavenc="07/08/2014";
+        pago.fechapago="30/07/2014";
+        pago.nombre = "JUAN PEREZ";
+        pagos.add(pago);
+
+        pago.anno = "2014";
+        pago.codigo="JPEREZ";
+        pago.itemno = 8;
+        pago.mes = "SETIEMBRE";
+        pago.concepto="PENSION";
+        pago.estado ="CANCELADO";
+        pago.monto = 150;
+        pago.fechavenc="07/09/2014";
+        pago.fechapago="30/08/2014";
+        pago.nombre = "JUAN PEREZ";
+        pagos.add(pago);
+
+        pago.anno = "2014";
+        pago.codigo="JPEREZ";
+        pago.itemno = 9;
+        pago.mes = "OCTUBRE";
+        pago.concepto="PENSION";
+        pago.estado ="CANCELADO";
+        pago.monto = 150;
+        pago.fechavenc="07/10/2014";
+        pago.fechapago="30/09/2014";
+        pago.nombre = "JUAN PEREZ";
+        pagos.add(pago);
+
+        pago.anno = "2014";
+        pago.codigo="JPEREZ";
+        pago.itemno = 10;
+        pago.mes = "MOVIEMBRE";
+        pago.concepto="PENSION";
+        pago.estado ="CANCELADO";
+        pago.monto = 150;
+        pago.fechavenc="07/11/2014";
+        pago.fechapago="30/10/2014";
+        pago.nombre = "JUAN PEREZ";
+        pagos.add(pago);
+
+        pago.anno = "2014";
+        pago.codigo="JPEREZ";
+        pago.itemno = 11;
+        pago.mes = "DICIEMBRE";
+        pago.concepto="PENSION";
+        pago.estado ="CANCELADO";
+        pago.monto = 170;
+        pago.fechavenc="07/12/2014";
+        pago.fechapago="30/11/2014";
+        pago.nombre = "JUAN PEREZ";
+        pagos.add(pago);
+
+        return vgson.toJson(pagos);
+    }
+  
     /**
      * PUT method for updating or creating an instance of PagoResource
      * @param content representation for the resource
