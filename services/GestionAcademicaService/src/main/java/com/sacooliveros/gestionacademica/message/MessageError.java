4d1391d0dd05872a771159d8cc1989c8bfa4391a
ricardo.castillejo <ricardo.castillejo@gmail.com>
2014-11-19 19:50:42 +0000

    Se carga las fuentes de los servicios:
    Se agrego el cliente web service de alumno de Saco Oliveros
    Se adecua el servicio GestionAcademicaService para el consumo del servicio de alumno
    Se separo los bean que se mapean tanto para la aplicaicon movil como el servicio de gestionacademica para su reusabilidad

---
+++ services/GestionAcademicaService/src/main/java/com/sacooliveros/gestionacademica/message/MessageError.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.sacooliveros.gestionacademica.message;
+
+/**
+ *
+ * @author Ricardo
+ */
+public interface MessageError {
+    
+    String GET_ALUMNO = "Error al obtener alumno:";
+    String LOGIN = "Error al realizar login:";
+    
+}
